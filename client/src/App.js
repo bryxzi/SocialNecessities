2521ac6c6d066d561e4d79784d3be6b1a4278563
Keyur Bisetty <keyur@Keyurs-MacBook-Pro.local>
2023-04-03 19:28:55 -0400

    added packages and webpack config

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,9 @@ import { Container } from 'semantic-ui-react';
 
 import Home from './components/Home.js';
 import Login from './components/Login.js';
-import MenuBar from './components/MenuBar.js';
-import DetailPage from './components/PostDetail.js';
-import Register from './components/Register.js';
+import MenuBar from './components/Nav.js';
+import DetailPage from './components/PostInfo.js';
+import Register from './components/Signup.js';
 import { AuthProvider } from './auth/auth';
 import AuthRoute from './utils/AuthRoute';
 
---
+++ client/webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+              name: "[name].[ext]",
+              outputPath: "assets/fonts/"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
+  
+};
