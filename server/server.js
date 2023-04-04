fcea25a0ff624847709848c0e9527e9ffaa5bc4e
Keyur Bisetty <keyur@Keyurs-MacBook-Pro.local>
2023-04-04 00:57:51 -0400

    .env

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: '../.env' });
+dotenv.config({ path: './.env' });
 
 const { createServer } = require('http');
 const { subscribe, execute } = require('graphql');
