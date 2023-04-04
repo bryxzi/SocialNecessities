29fdfd134db547731fb8315851276aa16a45730c
Keyur Bisetty <keyur@Keyurs-MacBook-Pro.local>
2023-04-04 00:18:36 -0400

    changed port to 3000

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const path = require('path');
 const resolvers = require('./schema');
 const typeDefs = require('./schema/typeDefs');
 const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.DB_URI).then(() => {
   console.log('DB connected!!');
