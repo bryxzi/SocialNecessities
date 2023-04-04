136e72a407580c6764ade188af3fcc77342b096c
Keyur Bisetty <keyur@Keyurs-MacBook-Pro.local>
2023-04-04 00:03:42 -0400

    updated server.js for build path files

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: '../.env' }); 
+dotenv.config({ path: '../.env' });
 
 const { createServer } = require('http');
 const { subscribe, execute } = require('graphql');
@@ -9,6 +9,7 @@ const { PubSub } = require('graphql-subscriptions');
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const resolvers = require('./schema');
 const typeDefs = require('./schema/typeDefs');
@@ -16,50 +17,58 @@ const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.DB_URI).then(() => {
-    console.log('DB connected!!');
+  console.log('DB connected!!');
 }).catch(err => console.log(err));
 
 (async (typeDefs, resolvers) => {
-    const app = express();
-    const schema = makeExecutableSchema({ typeDefs, resolvers });
-    const httpServer = createServer(app);
-    const pubSub = new PubSub();
+  const app = express();
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+  const httpServer = createServer(app);
+  const pubSub = new PubSub();
 
-    const server = new ApolloServer({ 
-        schema,
-        context: ({req}) => ({ req, pubSub }),
-        plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), {
-            async serverWillStart() {
-                return {
-                    async drainServer() {
-                        subscriptionServer.close();
-                    }
-                }
-            }
-        }],
-    });
+  // Serve the static files from the React app
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
-    const subscriptionServer = SubscriptionServer.create({
-        schema,
-        execute,
-        subscribe,
-        async onConnect() {
-            console.log('Connected!');
-            return {
-                pubSub
-            }
-        },
-        onDisconnect() {
-            console.log('Disconnected!');
+  const server = new ApolloServer({
+    schema,
+    context: ({ req }) => ({ req, pubSub }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            subscriptionServer.close();
+          }
         }
-    }, {
-        server: httpServer,
-        path: server.graphqlPath
-    })
+      }
+    }],
+  });
 
-    await server.start();
-    server.applyMiddleware({ app });
+  const subscriptionServer = SubscriptionServer.create({
+    schema,
+    execute,
+    subscribe,
+    async onConnect() {
+      console.log('Connected!');
+      return {
+        pubSub
+      }
+    },
+    onDisconnect() {
+      console.log('Disconnected!');
+    }
+  }, {
+    server: httpServer,
+    path: server.graphqlPath
+  })
 
-    await new Promise(resolve => httpServer.listen({ port: PORT }, resolve));
-    console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+  await server.start();
+  server.applyMiddleware({ app });
+
+  // Handles any requests that don't match the ones above
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+
+  await new Promise(resolve => httpServer.listen({ port: PORT }, resolve));
+  console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`);
 })(typeDefs, resolvers);
